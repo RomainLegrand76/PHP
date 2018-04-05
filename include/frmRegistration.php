d2a30e60b073803775954f4b95c9088aabb5817e
RomainLegrand76 <legrand.romain76@sfr.fr>
2018-04-05 09:28:30 +0200

    Formulaire Inscription

---
+++ include/frmRegistration.php
@@ -0,0 +1,23 @@
+<form method="post" action="#">
+    <div>
+        <label for="nom">Nom&nbsp;:</label>
+        <input type="text" name="nom" />
+    </div>
+    <div>
+        <label for="prenom">Prénom&nbsp;:</label>
+        <input type="text" name="prenom" />
+    </div>
+    <div>
+        <label for="mail">Email&nbsp;:</label>
+        <input type="text" name="mail" />
+    </div>
+    <div>
+        <label for="mdp">Mot de passe&nbsp;:</label>
+        <input type="password" name="mdp" />
+    </div>
+    <div>
+        <input type:"reset" value="Effacer"/>
+        <input type:"submit" value="Valider"/>
+    </div>
+    <input type="hidden" name="frmRegistration"/>
+</form>
